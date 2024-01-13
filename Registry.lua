d1d0284a4ccaac58bebaa5a897fac314f09a3f65
rrhiemstar <rrhiemstar@gmail.com>
2024-01-13 15:23:21 +0100

    Initialized new registry.

---
+++ Registry.lua
@@ -0,0 +1,8 @@
+Registry = {
+    name = "MyRegistry",
+    uuid = "1db47c2e-f6d8-4db3-845d-a645b11efa19",
+    url  = "git@github.com:renehiemstra/MyRegistry.git",
+    description = "MyRegistry local package registry",
+    packages = {}
+}
+return Registry
