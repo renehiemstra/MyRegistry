da7d578e4b98bb911804e5162b80d36302e095fb
rrhiemstar <rrhiemstar@gmail.com>
2024-01-13 15:33:37 +0100

    <new package> Dep1

---
+++ D/Dep1/0.1.0/Specs.lua
@@ -0,0 +1,11 @@
+Project = {
+    name = "Dep1",
+    uuid = "fb04da88-e7da-4783-a160-8cb18c3a0ab4",
+    version = "0.1.0",
+    url  = "git@github.com:renehiemstra/Dep1.git",
+    sha1 = "09cadeaa8b8c7bed45196ad873e2f12b6ed282c8",
+    deps = {
+        DepDep = "0.1.0",
+    },
+}
+return Project
--- Registry.lua
+++ Registry.lua
@@ -3,6 +3,11 @@ Registry = {
     uuid = "1db47c2e-f6d8-4db3-845d-a645b11efa19",
     url  = "git@github.com:renehiemstra/MyRegistry.git",
     description = "MyRegistry local package registry",
-    packages = {}
+    packages = {
+        Dep1 = {
+            path = "D/Dep1",
+            uuid = "fb04da88-e7da-4783-a160-8cb18c3a0ab4",
+        },
+    },
 }
 return Registry
